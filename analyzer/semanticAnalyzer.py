bcd3739476bfab8bb35c13192286eff6ecac66d6
creeper <2907692855@qq.com>
2024-01-15 18:12:10 +0800

    modified:   analyzer/semanticAnalyzer.py

--- analyzer/semanticAnalyzer.py
+++ analyzer/semanticAnalyzer.py
@@ -6,7 +6,7 @@ class SemanticAnalyzer:
         self.errorHandler = errorHandler
 
     def parse_S(self, node):
-        children = self.parse_tree.children(node.tag)
+        children = self.parse_tree.children(node.identifier)
         for child in children:
             if child.tag == 'line':
                 self.parse_line(child)
@@ -14,17 +14,8 @@ class SemanticAnalyzer:
                 self.parse_S(child)
 
     def parse_line(self, node):
-        children = self.parse_tree.children(node.tag)
-        for child in children:
-            if child.tag == 'assign':
-                self.parse_assign(child)
-            elif child.tag == 'print':
-                self.parse_print(child)
-            elif child.tag == 'if':
-                self.parse_if(child)
-            elif child.tag == 'while':
-                self.parse_while(child)
+        children = self.parse_tree.children(node.identifier)
 
     def analyze(self, parse_tree):
         self.parse_tree = parse_tree
-        self.parse_S(parse_tree.root)
+        self.parse_S(parse_tree[parse_tree.root])
